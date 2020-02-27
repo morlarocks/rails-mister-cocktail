b7efec39e8164f95569cef5c011dce1c1c26a333
Birgit Klink <birgit@morla.rocks>
2020-02-27 17:36:17 +0800

    Challenge 2 done almost

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  # get '/cocktails', to: 'cocktails#index', as: 'cocktails'
+  # get '/cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
+  # post '/cocktails', to: 'cocktails#create'
+  # get '/cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+
+  # get 'cocktails/:id/doses/new', to: 'doses#new', as: 'new_dose'
+  # post 'cocktails/:id/doses', to: 'doses#create'
+  # delete '/doses/:id', to: 'doses#destroy'
+
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,39 @@
+require 'faker'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts 'Creating new database...'
+
+URL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+data = JSON.parse(open(URL).read)
+ingredients = []
+data['drinks'].each do |item|
+  ingredients << item['strIngredient1']
+end
+
+15.times do
+  cocktail = Cocktail.new(name: Faker::Dessert.unique.flavor)
+  cocktail.save
+  3.times do
+    ingredient = Ingredient.new(name: ingredients.sample)
+    ingredient.save
+
+    dose = Dose.new(
+      description: Faker::Food.measurement,
+      ingredient_id: ingredient.id,
+      cocktail_id: cocktail.id
+    )
+    dose.save
+  end
+  cocktail.save
+end
+
+puts 'Database created'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
